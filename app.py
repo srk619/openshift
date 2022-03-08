2f665d02ff04411ae06fe06c31c9de11fc1b38d4
Sharath Kumar R <microtraningskr@gmail.com>
2022-03-08 15:23:59 +0530

    commit3

--- app.py
+++ app.py
@@ -8,34 +8,20 @@ import os
 app = Flask(__name__)
 
 color_codes = {
-    "red": "#e74c3c",
-    "green": "#16a085",
-    "blue": "#2980b9",
-    "pink": "#be2edd",
-    "yellow": "#ffff00",
+    "red": "#FF0000",
+    "green": "#00FF00",
+    "blue": "#0000FF",
     "white": "#ffffff",
-    "purple": "#7d3c98"
+    "black": "#0000000"
 }
 
-color = os.environ.get('APP_COLOR') or random.choice(["green","green"])
-#color = os.environ.get('APP_COLOR') or random.choice(["white"])
-#color = os.environ.get('APP_COLOR') or random.choice(["pink","blue","yellow"])
+color = os.environ.get('APP_COLOR') or random.choice(["red","green","blue"])
 
 @app.route("/")
 def main():
     #return 'Hello'
     print(color)
     return render_template('hello.html', name=socket.gethostname(), color=color_codes[color])
-
-@app.route('/color/<new_color>')
-def new_color(new_color):
-    return render_template('hello.html', name=socket.gethostname(), color=color_codes[new_color])
-
-@app.route('/read_file')
-def read_file():
-    f = open("/data/testfile.txt")
-    contents = f.read()
-    return render_template('hello.html', name=socket.gethostname(), contents=contents, color=color_codes[color])
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="8080")
